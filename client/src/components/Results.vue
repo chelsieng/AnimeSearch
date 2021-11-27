892aec1d28e4c170813e269de3b4e57f94fef61a
chelsieng <chelsiengmk@gmail.com>
2021-11-27 14:16:01 -0500

    results

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -7,20 +7,51 @@
     <div class="card">
       <div class="card-body">
         <div class="row mb-3 justify-content-between" v-for="r in this.$route.query.result" :key="r">
-          <h5>{{ r['English Title'] }}</h5>
+          <h3>{{ r['English Title'] }}</h3>
           <div class="col-lg-3">
             <img :src="r['Image_url']"
                  class="rounded" alt="">
           </div>
           <div class="col-lg-9">
-            <p class="card-text">Description: {{ r['description'] }}</p>
-            <p class="card-title"> Rating: {{ r['rating'] }}</p>
-            <p class="card-text">Number of episodes: {{ r['number_episodes'] }}</p>
-            <p class="card-text">Duration: {{ r['duration_per_eps(min)'] }}</p>
-            <p class="card-text">Popularity: {{ r['popularity'] }}</p>
-            <p class="card-text">Status: {{ r['Status'] }}</p>
-            <p class="card-text">Source: {{ r['Source'] }}</p>
-            <span>Genre: </span><span v-for="g in r['genre']" :key="g" class="card-text"> {{ g }} </span>
+            <h5>Description: </h5>
+            <p>{{ r['description'] }}</p>
+            <div class="row mb-3">
+              <div class="col-4">
+                <p class="card-text fw-bold"> Rating:
+                  <span v-if="r['rating'] >= 6" class="text-success">{{ r['rating'] }}</span>
+                  <span v-else class="text-danger">{{ r['rating'] }}</span></p>
+              </div>
+              <div class="col-4">
+                <p class="card-text fw-bold">Popularity: <span class="fw-normal">{{ r['popularity'] }}</span></p>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-4">
+                <p class="card-text fw-bold">Duration:
+                  <span v-if="r['duration_per_eps(min)'] === 0" class="fw-normal"> ? min/ep</span>
+                  <span v-else class="fw-normal">{{ r['duration_per_eps(min)'] }} min/ep</span>
+                </p>
+              </div>
+              <div class="col-4">
+                <p class="card-text fw-bold">Number of episodes:
+                  <span v-if="r['number_episodes'] === 'Unknown'" class="fw-normal"> ? </span>
+                  <span v-else class="fw-normal">{{
+                    r['number_episodes']
+                  }}</span></p>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-4">
+                <p class="card-text fw-bold">Status: <span class="fw-normal">{{ r['Status'] }}</span></p>
+              </div>
+              <div class="col-4">
+                <p class="card-text fw-bold">Source: <span class="fw-normal">{{ r['Source'] }}</span></p>
+              </div>
+            </div>
+            <span class="fw-bold">Genre: </span><span v-for="i in r['genre'].length" :key="i" class="card-text">
+                <span v-if="i === r['genre'].length">{{ r['genre'][i - 1] }}</span>
+                <span v-else>{{ r['genre'][i - 1] }}, </span>
+              </span>
           </div>
         </div>
       </div>
